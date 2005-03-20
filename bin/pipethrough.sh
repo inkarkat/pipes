bc8c07331ada32b2a535cd2a09e17a0322d53d50
Ingo Karkat <swdev@ingo-karkat.de>
2005-03-21 00:00:00 +0100

    BF: invoking ${command} via shell, because commands with input redirection (e.g. "tr '\\' '/' <") were hanging

--- bin/pipethrough.sh
+++ bin/pipethrough.sh
@@ -1,19 +1,30 @@
 #!/bin/sh
-
+###########################################################################HP##
+##
+# FILE: 	
+# PRODUCT:	
+# AUTHOR: 	
+# DATE CREATED:	
+#
 ###############################################################################
-#	PURPOSE:
+# PURPOSE:
 #	Pipes the (list of) files through an external command and 
 #	writes the output back into the file.
-#	ASSUMPTIONS	/ PRECONDITIONS:
+# ASSUMPTIONS / PRECONDITIONS:
 #	?? List of any external variable, control, or other element whose state affects this procedure.
-#	EFFECTS		/ POSTCONDITIONS:
+# EFFECTS / POSTCONDITIONS:
 #	?? List of the procedure's effect on each external variable, control, or other element.
-#	INPUTS:
+# INPUTS:
 #	first argument: command (e.g. dos2ux)
 #	next arguments: files to be modified
-#	RETURN VALUES: 
+# RETURN VALUES: 
 #	none
+# REVISION	DATE		REMARKS 
+#	0.10	01-Jun-2004	BF: invoking ${command} via shell, because
+#	commands with input redirection (e.g. "tr '\\' '/' <") were hanging. 
+#	0.01	00-Jan-2001	file creation
 ###############################################################################
+
 if [ $# -lt 2 ]; then
 	echo "Usage: `basename $0` <command> file [, ...]"
 	exit 1
@@ -25,5 +36,7 @@ shift
 for file do
 	tempdestination=${file}.$$
 	echo "${command} "'"'"${file}"'"'
-	${command} "${file}" > "${tempdestination}" && mv "${tempdestination}" "${file}"
+	sh -c "${command} \"${file}\" > \"${tempdestination}\" && mv \"${tempdestination}\" \"${file}\""
+	####- ${command} "${file}" > "${tempdestination}" && mv "${tempdestination}" "${file}"
 done
+
